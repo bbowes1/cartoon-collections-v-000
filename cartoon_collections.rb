712ca0f2ada30a9cd8661a9da78d5a3749d770b1
Benjamin Bowes <bbowes999@gmail.com>
2018-03-26 13:52:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-cheese_types.detect { |x| x
+cheese_types.detect { |x| x}
 if array.include?("cheese_types")
 x
  else
